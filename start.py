7a55a31c16336f8c14ea1bde9636f341b4f17be4
guyao <gamesgao@163.com>
2016-12-13 23:30:41 +0800

    好像光流法有些问题

--- start.py
+++ start.py
@@ -7,7 +7,7 @@ import IO
 from graph_segmentation import *
 
 if len(sys.argv) == 1:
-    readSource = '13.mp4'
+    source = readSource = '13.mp4'
 else:
     source = sys.argv[1]
 # groundTruth = False
