088b601bb1bade320eef9d912be6a1c9a11135cd
guyao <gamesgao@163.com>
2017-01-02 11:17:15 +0800

    修改参数

--- start.py
+++ start.py
@@ -19,7 +19,7 @@ flowVideo = IO.readFlowVideoFromFile("./flowTemp/")
 assert video.getFrameNumber() > 0
 assert video.getFrameNumber() == flowVideo.getFrameNumber()
 M = 300
-L = 10
+L = 9
 c = 0.2
 beta = 0.2
 alpha = 1 - beta
